97d1d4a65580e84ff88d5db104aa7b5158534949
Patrick Oweijane <77375871+PatrickOweijane@users.noreply.github.com>
2022-04-03 17:36:02 -0500

    Spelling mistake corrected

--- run_all_pairs_shortest_paths.py
+++ run_all_pairs_shortest_paths.py
@@ -3,8 +3,8 @@ from shortest_path.all_pairs.slow import slow
 from shortest_path.all_pairs.fast import fast
 
 
-print('Using slow dynamic programing approach O(V^4) time:')
+print('Using slow dynamic programming approach O(V^4) time:')
 slow(GRAPH)
 
-print('Using fast dynamic programing approach O(V^3 lgV) time:')
+print('Using fast dynamic programming approach O(V^3 lgV) time:')
 fast(GRAPH)
