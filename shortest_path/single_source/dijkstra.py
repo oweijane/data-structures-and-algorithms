46a469bcfb12c143c42baf2e3105a2982d5ef267
Patrick Oweijane <77375871+PatrickOweijane@users.noreply.github.com>
2022-04-03 21:11:48 -0500

    Added predecessor

--- shortest_path/single_source/dijkstra.py
+++ shortest_path/single_source/dijkstra.py
@@ -8,14 +8,16 @@ def dijkstra(adjacency_matrix, source):
     '''
     number_of_vertices = len(adjacency_matrix)
     shortest_d = [inf] * number_of_vertices
+    predecessor = [None] * number_of_vertices
     seen = [False] * number_of_vertices
-    heap = [(0, source)]
+    heap = [(0, source, None)]
     while heap:
-        d, v = heappop(heap)
+        d, v, pred = heappop(heap)
         if seen[v]: continue
         seen[v] = True
         shortest_d[v] = d
+        predecessor[v] = pred
         for u, weight in enumerate(adjacency_matrix[v]):
             if weight == inf or seen[u]: continue
-            heappush(heap, (d + weight, u))
-    return shortest_d
+            heappush(heap, (d + weight, u, v))
+    return shortest_d, predecessor
