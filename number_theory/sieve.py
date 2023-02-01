4ca5ce9c8d8951f9133f0315f97365755d4e9231
Patrick Oweijane <oweijane@patricks-air.cbu.edu>
2023-01-31 21:28:32 -0600

    Added Sieve of Eratosthenes

---
+++ number_theory/sieve.py
@@ -0,0 +1,10 @@
+def sieve_optimal(N):
+    res = [True] * (N + 1)
+    res[0] = res[1] = False
+    i = 2
+    while i * i <= N:
+        if res[i]:
+            for j in range(i * i, N + 1, i):
+                res[j] = False
+        i += 1
+    return res
